734d2ea9852df59e4953a9bc84e542eb8c83d9a3
marte <mipcomputacion@gmail.com>
2021-04-10 11:24:50 -0300

    rutas-securityModel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultComponent } from './public/home/default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: DefaultComponent
+  },
+  {
+    path:'',
+    pathMatch:'full',
+    redirectTo: '/home'
+  },
+  /*** this must be the last one */
+  {
+    path: '**',
+    redirectTo: '/'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
